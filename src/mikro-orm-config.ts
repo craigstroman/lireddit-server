b14e7c1ac4a6a34181651cafd90dc9b9f65b5224
Craig Stroman <cstroman@gmail.com>
2025-07-22 15:51:05 -0400

    Updating configuration for mikro orm

--- src/mikro-orm-config.ts
+++ src/mikro-orm-config.ts
@@ -1,17 +1,21 @@
 import path from 'path';
 import { MikroORM } from '@mikro-orm/core';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import dotenv from 'dotenv';
 import { Post } from './entities/POST';
 import { __prod__ } from './constants';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default {
   migrations: {
     path: path.join(__dirname, './migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
+  driver: PostgreSqlDriver,
+  dbName: process.env.DB_NAME,
   entities: [Post],
-  type: 'postgresql',
-  dbName: 'portfolio',
-  user: 'craig125924',
-  password: '0SB03cyeH1nB563sjmh2x',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
