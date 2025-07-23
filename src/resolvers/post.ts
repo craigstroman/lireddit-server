e0710fbf5b79d5ef84a72d297ea22885b1a731b1
Craig Stroman <cstroman@gmail.com>
2025-07-23 16:07:22 -0400

    Adding find and create to resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
 import { Post } from '../entities/POST';
 import { MyContext } from 'src/types';
 
@@ -7,5 +7,27 @@ export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, {
+      title,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    await em.persistAndFlush(post);
+    return post;
   }
 }
