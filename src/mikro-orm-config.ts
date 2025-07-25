cad3859a589f745394b416d95bb3afd97e32f3bf
Craig Stroman <cstroman@gmail.com>
2025-07-25 13:58:10 -0400

    Updating mikro orm config

--- src/mikro-orm-config.ts
+++ src/mikro-orm-config.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import { Post } from './entities/POST';
 import { __prod__ } from './constants';
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 export default {
   migrations: {
@@ -19,7 +19,7 @@ export default {
   password: process.env.DB_PASSWORD,
   debug: !__prod__,
   allowGlobalContext: true,
-  // transactional: true, // run each migration inside transaction
-  // allOrNothing: true, // run all migrations in current batch in master transaction
-  // emit: 'ts', // migration generation mode
+  transactional: true, // run each migration inside transaction
+  allOrNothing: true, // run all migrations in current batch in master transaction
+  emit: 'ts', // migration generation mode
 } as Parameters<typeof MikroORM.init>[0];
