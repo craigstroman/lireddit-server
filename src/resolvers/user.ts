906c0e1c9e578777edda9263e87a1911b3c46fb4
Craig Stroman <cstroman@gmail.com>
2025-07-26 15:20:51 -0400

    Creating login mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -7,6 +7,7 @@ import {
   InputType,
   Field,
   Ctx,
+  ObjectType,
 } from 'type-graphql';
 import argon2 from 'argon2';
 import { User } from '../entities/USER';
@@ -17,6 +18,23 @@ class UsernamePasswordInput {
   username: string;
   @Field()
   password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
@@ -35,5 +53,43 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {
+      username: options.username.toLowerCase(),
+    });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: "Username doesn't exist",
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: "Password doesn't match",
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
   }
 }
