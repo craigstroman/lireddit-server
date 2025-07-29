69c9987c8abed23adaa2426b8821fbe432714393
Craig Stroman <cstroman@gmail.com>
2025-07-29 13:23:36 -0400

    Updating types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
+import { Request } from 'express';
 import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core';
 
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request;
+  res: Response;
 };
