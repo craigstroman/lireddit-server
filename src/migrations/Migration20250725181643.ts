d33b9db2ec691f68df9a7b181eb81516e9e48414
Craig Stroman <cstroman@gmail.com>
2025-07-31 15:47:11 -0400

    Adding columns for user to migration

--- src/migrations/Migration20250725181643.ts
+++ src/migrations/Migration20250725181643.ts
@@ -3,7 +3,7 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20250725181643 extends Migration {
   override async up(): Promise<void> {
     this.addSql(
-      'create table "user" ("id" serial primary key, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "username" text not null, "password" text not null);'
+      'create table "user" ("id" serial primary key, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, first_name text not null, last_name text not null, email text not null, "username" text not null, "password" text not null);'
     );
     this.addSql(
       'alter table "user" add constraint "user_username_unique" unique ("username");'
