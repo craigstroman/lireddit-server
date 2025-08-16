3a1b80b6c9f17d707b2a60cafb53f5acc683e8f6
Craig Stroman <cstroman@gmail.com>
2025-08-16 13:18:59 -0400

    Updating user resolver to handle username or email

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -8,9 +8,14 @@ import {
   ObjectType,
   Query,
 } from 'type-graphql';
+import { v4 } from 'uuid';
 import argon2 from 'argon2';
 import { MyContext } from 'src/types';
 import { User } from '../entities/USER';
+import { sendEmail } from '../utils/sendEmail';
+import { FORGET_PASSWORD_PREFIX } from '../constants';
+
+// TODO: Also add email validation to server side
 
 @InputType()
 class UsernameRegisterInput {
@@ -65,6 +70,34 @@ export class UserResolver {
     const user = await em.findOne(User, { id });
 
     return user;
+  }
+
+  @Mutation(() => Boolean)
+  async forgotPassword(
+    @Arg('email') email: string,
+    @Ctx() { req, em, redis }: MyContext
+  ) {
+    const user = await em.findOne(User, { email });
+
+    if (!user) {
+      return true;
+    }
+
+    const token = v4();
+
+    await redis.set(
+      FORGET_PASSWORD_PREFIX + token,
+      user.id,
+      'ex',
+      1000 * 60 * 60 * 24 * 3
+    ); // 3 days
+
+    await sendEmail(
+      email,
+      `<a href="http://localhost:3000/change-password/${token}">reset password</a>`
+    );
+
+    return true;
   }
 
   @Mutation(() => UserResponse)
@@ -127,12 +160,16 @@ export class UserResolver {
 
   @Mutation(() => UserResponse)
   async login(
-    @Arg('options') options: UsernameLoginInput,
+    @Arg('usernameOrEmail') usernameOrEmail: string,
+    @Arg('password') password: string,
     @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
-    const user = await em.findOne(User, {
-      username: options.username.toLowerCase(),
-    });
+    const user = await em.findOne(
+      User,
+      usernameOrEmail.includes('@')
+        ? { email: usernameOrEmail }
+        : { username: usernameOrEmail }
+    );
 
     if (!user) {
       return {
@@ -145,7 +182,7 @@ export class UserResolver {
       };
     }
 
-    const valid = await argon2.verify(user.password, options.password);
+    const valid = await argon2.verify(user.password, password);
 
     if (!valid) {
       return {
