a79dd2ceb4616597140c2c38399551618c49cd90
Craig Stroman <cstroman@gmail.com>
2025-11-12 15:20:24 -0500

    Adding error checking for deleting a post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -208,6 +208,15 @@ export class PostResolver {
     @Arg('id', () => Int) id: number,
     @Ctx() { req }: MyContext
   ): Promise<boolean> {
+    const post = await Post.findOne(id);
+
+    if (!post) {
+      return false;
+    }
+    if (post.creatorId !== req.session.userId) {
+      throw new Error('not authorized');
+    }
+
     await Post.delete({ id, creatorId: req.session.userId });
     return true;
   }
