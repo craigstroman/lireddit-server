d797e6c08d951a0bc6cc332d0fb790426a470290
Craig Stroman <cstroman@gmail.com>
2025-11-19 15:14:23 -0500

    Updating post migration

--- src/migrations/1758653732832-post.ts
+++ src/migrations/1758653732832-post.ts
@@ -13,15 +13,13 @@ export class post1758653732832 implements MigrationInterface {
                     points integer NOT NULL DEFAULT 0,
                     "createdAt" timestamp without time zone NOT NULL DEFAULT now(),
                     "updatedAt" timestamp without time zone NOT NULL DEFAULT now(),
+                    "voteStatus" integer NOT NULL DEFAULT 0,
                     CONSTRAINT "PK_be5fda3aac270b134ff9c21cdee" PRIMARY KEY (id),
                     CONSTRAINT "FK_9e91e6a24261b66f53971d3f96b" FOREIGN KEY ("creatorId")
                         REFERENCES public."user" (id) MATCH SIMPLE
                         ON UPDATE NO ACTION
                         ON DELETE NO ACTION
                 )
-
-                ALTER TABLE public.post 
-                ADD COLUMN "voteStatus" integer NOT NULL DEFAULT 0,
             `
     );
   }
