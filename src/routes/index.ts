26b6a1699aa46caa78af2fe445f77b7c2d42a5c0
Craig Stroman <cstroman@gmail.com>
2025-11-20 12:35:35 -0500

    Updating routes so refresh works on dashboard page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,6 +11,8 @@ router.route('/').get(indexPage);
 
 router.route('/register').get(indexPage);
 
+router.route('/dashboard').get(indexPage);
+
 router.route('/login').get(indexPage);
 
 router.route('/change-password/').get(indexPage);
