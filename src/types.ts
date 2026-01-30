96c4d9b3c045f6e78997a0b1db2572ec364eb301
Craig Stroman <cstroman@gmail.com>
2026-01-30 14:49:20 -0500

    Updating types configuration

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,12 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { Redis } from 'ioredis';
 import { createUserLoader } from './utils/createUserLoader';
 import { createUpdootLoader } from './utils/createUpdootLoader';
 
-interface Request {
-  session?: Session & Partial<SessionData>;
-  url: string;
-  hostname: string;
-  params: object;
-  headers: object;
-  cookies: object;
-  body: object;
-  is: string;
-  ip: string;
-  method: string;
-  query: object;
-  protocol: string;
-  originalUrl: string;
-  accepts: string | boolean;
-}
-
 export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request;
-  res: Response;
+  req: Request & { session: Express.Session };
   redis: Redis;
+  res: Response;
   userLoader: ReturnType<typeof createUserLoader>;
   updootLoader: ReturnType<typeof createUpdootLoader>;
 };
